346d484af0dde93940b6ec477581ef1cfdcc9911
Corentin Desilles <corentindesilles@air-de-corentin.home>
2023-02-04 18:17:36 +0100

    <debut_config_router>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-// import HomePage from './pages/HomePage/HomePage';
-
-import AboutPage from './pages/AboutPage/AboutPage';
+import HomePage from './pages/HomePage/HomePage';
+import Header from './components/Header/Header';
+import style from './pages/HomePage/HomePage.module.scss';
+// import AboutPage from './pages/AboutPage/AboutPage';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return <AboutPage />;
-  // <HomePage />;
+  return (
+    <div className={`${style.homepageLayout}`}>
+      <Header />
+      <HomePage />
+      {/* <AboutPage /> */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,17 +1,12 @@
-import Header from '../../components/Header/Header';
-import Banner from './components/Banner/Banner';
+import Banner from '../../components/Banner/Banner';
 import Apartment from './components/Apartment/Apartment';
-import Footer from '../../components/Footer/Footer';
-import style from './HomePage.module.scss';
 
 function HomePage() {
   return (
-    <div className={`${style.homepageLayout}`}>
-      <Header />
+    <>
       <Banner />
       <Apartment />
-      <Footer />
-    </div>
+    </>
   );
 }
 
