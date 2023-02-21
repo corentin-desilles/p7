80cae82e7e466a4e9e171c350d1045fcb7d3005d
Corentin Desilles <corentindesilles974@gmail.com>
2023-02-21 14:28:39 +0100

    underline_stay_when_active

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/images/LOGO.png';
 import style from './Header.module.scss';
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header>
       <Link to="/">
@@ -10,11 +12,17 @@ function Header() {
       </Link>
       <nav>
         <ul>
-          <Link to="/">
+          <Link
+            to="/"
+            className={location.pathname === '/' ? `${style.active}` : ''}
+          >
             <li className={`${style.link}`}>Accueil</li>
           </Link>
 
-          <Link to="/about">
+          <Link
+            to="/about"
+            className={location.pathname === '/about' ? `${style.active}` : ''}
+          >
             <li className={`${style.link}`}>A Propos</li>
           </Link>
         </ul>
